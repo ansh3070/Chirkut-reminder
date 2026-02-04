12ac4dcaec89e850de10a569861a35a89f8cca03
ANSH RAJPUT <150443945+ansh3070@users.noreply.github.com>
2026-02-04 20:03:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,17 +2,13 @@ self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker Active');
-});
-
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'NOTIFY') {
         self.registration.showNotification(event.data.title, {
             body: event.data.body,
             icon: 'https://via.placeholder.com/128/8ac6d1/ffffff?text=💙',
             vibrate: [200, 100, 200],
-            tag: 'chirkut-notification' // Prevents duplicate stacking
+            tag: 'chirkut-notif'
         });
     }
 });
